a7447730357cb65f4f509c5899f8cbd3c1856d55
小島和也 <kazuya@kojimakazuyanoMacBook-ea.local>
2020-06-17 15:35:38 +0900

    ActiveAdminを使った管理者画面の作成

---
+++ config/application.rb
@@ -0,0 +1,15 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+
+Bundler.require(*Rails.groups)
+
+module AdminSample0617
+  class Application < Rails::Application
+    config.load_defaults 6.0
+    config.i18n.default_locale = :ja
+
+
+  end
+end
